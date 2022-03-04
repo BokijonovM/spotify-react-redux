25f3f46cebcd2d55d1520eed225ee74b8056dc2c
Mukhsinjon Bokijonov <78298971+BokijonovM@users.noreply.github.com>
2022-03-04 19:58:50 +0500

    updated

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import Song from "./Song";
 import { Row } from "react-bootstrap";
 import { useParams } from "react-router-dom";
