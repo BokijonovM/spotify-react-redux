5ab5d73f45462c4449adca26c6c00be1f7a805de
Mukhsinjon Bokijonov <78298971+BokijonovM@users.noreply.github.com>
2022-03-06 07:06:27 +0500

    udated

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -12,7 +12,7 @@ export const ADD_TO_ALBUM_CART = "ADD_TO_ALBUM_CART";
 export const REMOVE_TO_ALBUM_CART = "REMOVE_TO_ALBUM_CART";
 export const ADD_TO_HOME_DISPLAY = "ADD_TO_HOME_DISPLAY";
 export const LOADING_WHILE_DISPLAY = "LOADING_WHILE_DISPLAY";
-export const SELECT_SONG = 'SELECT_SONG'
+export const SELECT_SONG = "SELECT_SONG";
 
 export const addToFavouritesArtistsAction = (artistToAdd) => ({
   type: ADD_TO_FAVOURITESARTISTS,
@@ -25,9 +25,9 @@ export const removeFromFavouritesArtistsAction = (indexToRemove) => ({
 });
 
 export const selectSongAction = (song) => ({
-    type: SELECT_SONG,
-    payload: song
-})
+  type: SELECT_SONG,
+  payload: song,
+});
 
 export const getArtistAction = (artistId) => {
   console.log("in getArtistAction");
@@ -157,41 +157,13 @@ export const getDataSearch = (query) => {
             payload: data,
           });
           dispatch({
-    "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
-    "X-RapidAPI-Key": "c74a0a086emshf55ffb8dbdcb59ap17a486jsnb83bb4d3e387",
-  });
-
-export const searchAction = (term)=>({
-    type: ADD_TO_HOME_DISPLAY,
-    payload: term
-}) 
-
-
-export const getDataSearch = (query) =>{
-    return (dispatch) => {
-  setTimeout( async()=>{
-   try {
-       const response = await fetch(
-     "https://striveschool-api.herokuapp.com/api/deezer/search?q=" +
-       query, {
-           method: 'GET',
-           headers
-       });
-       if(response.ok){
-           const {data} = await response.json()
-           dispatch({
-               type: ADD_TO_HOME_DISPLAY,
-               payload:data
-           })
-           dispatch({
-               type: LOADING_WHILE_DISPLAY,
-           })
-           console.log(data)
-       }else{
-        dispatch({
             type: LOADING_WHILE_DISPLAY,
           });
           console.log(data);
+        } else {
+          dispatch({
+            type: LOADING_WHILE_DISPLAY,
+          });
         }
       } catch (error) {
         console.log(error);
